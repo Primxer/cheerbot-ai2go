e93e278a3ca3e6fa89a0b936c93b68aa1cd0e78b
Primxer <rripley@uw.edu>
2019-10-12 22:15:49 -0700

    GPIO control for the LCD display

--- src/face.py
+++ src/face.py
@@ -1,7 +1,29 @@
+from PIL import Image
+import Adafruit_ILI9341 as TFT
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
 
 class RealFace:
-    def show(emotion):
-        # Your code here
+    def show(self, emotion):
+        #Raspberry pi config
+        DC = 17
+        RST = 23
+        SPI_PORT = 0
+        SPI_DEVICE = 0
+
+        #Create TFT LCD display class
+        disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+
+        #Initialize display
+        disp.begin()
+
+        image = Image.open("FILENAMEHERE")
+
+        #Resize the image and rotate to display
+        image = image.rotate(90).resize((240, 320))
+
+        #Draw the image on the display hardware
+        disp.display(image)
         pass
 
 # For running code without needing an LCD display
@@ -15,3 +37,7 @@ def CheerbotFace(mock=True):
         return MockFace()
     else:
         return RealFace()
+
+if __name__ == '__main__':
+    test = RealFace()
+    test.show("happy")
