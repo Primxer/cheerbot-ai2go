fa6d94bb03da2b24213460a93ab79b7432fd6c98
Geoffrey Powell-Isom <geoff@geferay.com>
2019-10-13 02:12:49 -0700

    added motor control pin setup and functions to control with

--- src/rover.py
+++ src/rover.py
@@ -1,10 +1,25 @@
 from time import sleep
+From RPi.GPIO import GPIO
 
 #######Pin Asssignments for the Motor Controller
 #RightMotorSpeed pin  5  //blue wire pi18
 #RightMotorDir   pin  0  //purple wire pi12
 #LeftMotorSpeed  pin  4  //gray wire pi13
 #LeftMotorDir    pin  2  //white wire pi19
+
+#######Pin Setup
+GPIO.setmode(GPIO.BCM)
+LOW = 0
+HIGH = 0
+rightDir = LOW
+rightSpeed = LOW
+leftDir = LOW
+leftSpeed = LOW
+
+GPIO.setup(rightDir,GPIO.OUT)
+GPIO.setup(rightSpeed,GPIO.OUT)
+GPIO.setup(leftDir,GPIO.OUT)
+GPIO.setup(leftSpeed,GPIO.OUT)
 
 
 # Skeleton for where the real rover code would be
@@ -40,3 +55,37 @@ if __name__ == '__main__':
     rover = CheerbotRover(mock=True)
     rover.react('happy')
     rover.react('fear')
+
+def forward()
+    GPIO.output(rightDir,GPIO.HIGH)
+    GPIO.output(leftDir,GPIO.HIGH)
+    GPIO.output(rightSpeed,GPIO.HIGH)
+    GPIO.output(leftSpeed,GPIO.HIGH)
+
+def backward()
+    GPIO.output(rightDir,GPIO.LOW)
+    GPIO.output(leftDir,GPIO.LOW)
+    GPIO.output(rightSpeed,GPIO.HIGH)
+    GPIO.output(leftSpeed,GPIO.HIGH)
+
+def right()
+    GPIO.output(rightDir,GPIO.LOW)
+    GPIO.output(leftDir,GPIO.HIGH)
+    GPIO.output(rightSpeed,GPIO.HIGH)
+    GPIO.output(leftSpeed,GPIO.HIGH)
+
+def left()
+    GPIO.output(rightDir,GPIO.HIGH)
+    GPIO.output(leftDir,GPIO.LOW)
+    GPIO.output(rightSpeed,GPIO.HIGH)
+    GPIO.output(leftSpeed,GPIO.HIGH)
+
+def stop()
+    GPIO.output(rightDir,GPIO.LOW)
+    GPIO.output(leftDir,GPIO.LOW)
+    GPIO.output(rightSpeed,GPIO.LOW)
+    GPIO.output(leftSpeed,GPIO.LOW)
+
+
+#####We must make sure the motors are stopped at the end
+stop()
